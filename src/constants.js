c798b07029c591ea96c0e3560b6b69ef26f0643b
yang1666 <77461451+yang1666@users.noreply.github.com>
2022-12-03 23:43:10 -0500

    Delete constants.js

--- src/constants.js
+++
@@ -1,4 +0,0 @@
-export const TOKEN_KEY = 'token';
-export const BASE_URL = 'https://around-dot-around-179500.uc.r.appspot.com';
-export const MAPS_API_KEY = 'AIzaSyDSafcrUzQvSpQeub2Hw-pPPTFeRp7qmoQ';
-
